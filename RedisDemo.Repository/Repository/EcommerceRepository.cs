57db6355e7c3397594e516d03ca34eb97f5ec382
JiaSyuan Li <jiasyuan@hotmail.com>
2020-11-22 11:36:10 +0800

    Add GetECommerceFromDB & GetECommerceFromRedis Method

--- RedisDemo.Repository/Repository/EcommerceRepository.cs
+++ RedisDemo.Repository/Repository/EcommerceRepository.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RedisDemo.Repository.DTO;
 
 namespace RedisDemo.Repository.Repository
 {
     public class EcommerceRepository
     {
-
+        public List<EcommerceDto> GetECommerce()
+        {
+        }
     }
 }
