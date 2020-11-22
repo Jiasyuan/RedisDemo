342172a25a8391066f49aabda89c8c050d925dd3
JiaSyuan Li <jiasyuan@hotmail.com>
2020-11-22 13:09:45 +0800

    Registered ECommerceRepository Service

--- RedisDemo.WebAPI/Startup.cs
+++ RedisDemo.WebAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RedisDemo.Repository.Repository;
+using RedisDemo.Repository.Repository.Interface;
 
 namespace RedisDemo.WebAPI
 {
@@ -25,7 +27,8 @@ namespace RedisDemo.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(); 
+            services.AddTransient<IECommerceRepository, EcommerceRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
