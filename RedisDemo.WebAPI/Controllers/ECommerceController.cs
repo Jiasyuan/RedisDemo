48181d2aebd90170b4c668791c75a87cba5b3d8b
JiaSyuan Li <jiasyuan@hotmail.com>
2020-11-22 13:19:40 +0800

    Add ECommerceController & ECommerceResponse

---
+++ RedisDemo.WebAPI/Controllers/ECommerceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedisDemo.Repository.Repository.Interface;
+
+namespace RedisDemo.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ECommerceController : ControllerBase
+    {
+        private IECommerceRepository _eCommerceRepository;
+
+        public ECommerceController(IECommerceRepository eCommerceRepository)
+        {
+            this._eCommerceRepository = eCommerceRepository;
+        }
+
+        
+    }
+}
