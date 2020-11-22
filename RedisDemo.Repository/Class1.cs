e7e6bcb14121e316e862b30910f6d686451a8fed
JiaSyuan Li <jiasyuan@hotmail.com>
2020-11-22 11:06:59 +0800

    Add  EcommerceDto

--- RedisDemo.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RedisDemo.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ RedisDemo.Repository/DTO/EcommerceDto.cs
@@ -0,0 +1,23 @@
+﻿namespace RedisDemo.Repository.DTO
+{
+    /// <summary>
+    /// 電商DTO
+    /// </summary>
+    public class EcommerceDto
+    {
+        /// <summary>
+        /// 電商ID
+        /// </summary>
+        public int ecommerce_Id { get; set; }
+
+        /// <summary>
+        /// 電商英文名稱
+        /// </summary>
+        public string en_name { get; set; }
+
+        /// <summary>
+        /// 電商名稱
+        /// </summary>
+        public string ch_name { get; set; }
+    }
+}
