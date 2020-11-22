7e0fe12109cfd6f9b55409a5a39e269b3708f451
JiaSyuan Li <jiasyuan@hotmail.com>
2020-11-22 12:28:34 +0800

    Add AzureDbConnectionHelper

---
+++ RedisDemo.Repository/Common/Helper/AzureDbConnectionHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using RedisDemo.Repository.Common.Interface;
+
+namespace RedisDemo.Repository.Common.Helper
+{
+    public class AzureDbConnectionHelper : IDatabaseConnectionHelper
+    {
+        private readonly string _connectionString;
+        private readonly IConfiguration _configuration;
+
+        public AzureDbConnectionHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            this._connectionString = _configuration.GetSection("ConnectionSetting")
+                .Value; ;
+        }
+
+        /// <summary>
+        /// Create DbConnection
+        /// </summary>
+        /// <returns></returns>
+        public IDbConnection Create()
+        {
+            var sqlConnection = new SqlConnection(_connectionString);
+            return sqlConnection;
+        }
+    }
+}
