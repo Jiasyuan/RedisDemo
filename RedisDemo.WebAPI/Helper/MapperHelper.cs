2625b5b1f4c2142a8117331c22a76d2e14f45ad4
JiaSyuan Li <jiasyuan@hotmail.com>
2020-11-26 21:51:18 +0800

    Add MapperHelper

---
+++ RedisDemo.WebAPI/Helper/MapperHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+
+namespace RedisDemo.WebAPI.Helper
+{
+    public class MapperHelper
+    {
+        /// <summary>
+        /// Mapper All Properties IgnoreAllNonExisting
+        /// </summary>
+        /// <typeparam name="TInPut"></typeparam>
+        /// <typeparam name="TOutPut"></typeparam>
+        /// <param name="inPut"></param>
+        /// <returns></returns>
+        public static TOutPut MapperProperties<TInPut, TOutPut>(TInPut inPut)
+        {
+            TOutPut outPut = default;
+            if (inPut != null)
+            {
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<TInPut, TOutPut>(MemberList.None);
+                });
+                config.AssertConfigurationIsValid();//←證驗應對
+                var mapper = config.CreateMapper();
+                outPut = mapper.Map<TOutPut>(inPut);
+            }
+            return outPut;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="InPut"></typeparam>
+        /// <typeparam name="OutPut"></typeparam>
+        /// <param name="inPut"></param>
+        /// <returns></returns>
+        public static IEnumerable<TOutPut> MapperProperties<TInPut, TOutPut>(IEnumerable<TInPut> inPut)
+        {
+            IEnumerable<TOutPut> outPut = default;
+            if (inPut != null && inPut.Any())
+            {
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<TInPut, TOutPut>(MemberList.None);
+                });
+                config.AssertConfigurationIsValid();//←證驗應對
+                var mapper = config.CreateMapper();
+                outPut = mapper.Map<IEnumerable<TOutPut>>(inPut);
+            }
+            return outPut;
+        }
+    }
+}
