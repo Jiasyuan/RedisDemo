92585ad7994c2961f5814445b6f661460385ec6d
JiaSyuan Li <jiasyuan@hotmail.com>
2020-11-26 21:50:52 +0800

    Add Cotract Project

---
+++ RedisRedisDemo.Contract/ECommerceResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace RedisRedisDemo.Contract
+{
+    public class ECommerceResponse
+    {
+        /// <summary>
+        /// Data From
+        /// </summary>
+        public string DataFrom { get; set; }
+
+        /// <summary>
+        /// Result List
+        /// </summary>
+        public List<EcommerceModel> Result { get; set; }
+    }
+}
---
+++ RedisRedisDemo.Contract/EcommerceModel.cs
@@ -0,0 +1,20 @@
+﻿namespace RedisRedisDemo.Contract
+{
+    public class EcommerceModel
+    {
+        /// <summary>
+        /// 電商ID
+        /// </summary>
+        public int ecommerce_Id { get; set; }
+
+        /// <summary>
+        /// 電商英文名稱
+        /// </summary>
+        public string en_name { get; set; }
+
+        /// <summary>
+        /// 電商名稱
+        /// </summary>
+        public string ch_name { get; set; }
+    }
+}
