a210d90cb5ae0f20418ed0eae0e05fff3db87c16
JiaSyuan Li <jiasyuan@hotmail.com>
2020-12-03 00:50:03 +0800

    Modify host

--- Test/RedisRedisDemo.Test.UseSingleServer/Program.cs
+++ Test/RedisRedisDemo.Test.UseSingleServer/Program.cs
@@ -11,16 +11,16 @@ namespace RedisRedisDemo.Test.UseSingleServer
     {
         static void Main(string[] args)
         {
-            string host = "";
+            string host = "https://localhost:5002/api";
             string methoud = "ECommerce";
             ConcurrentStack<ECommerceResponse> resultConcurrentStack = new ConcurrentStack<ECommerceResponse>();
-            Parallel.For(1, 10, (i, state) =>
+            Parallel.For(1, 11, (i, state) =>
             {
                 resultConcurrentStack.Push(ApiHelper.Api<ECommerceResponse>(host, EnumContentType.json, EnumApiMethodType.Get, methoud, "", ""));
             });
             var resultList = resultConcurrentStack.ToList();
-            int fromDB = resultList.Where(w => w.DataFrom == "Data From DB").Count();
-            int fromRedis = resultList.Where(w => w.DataFrom == "Data From Redis").Count();
+            int fromDB = resultList.Count(w => w.DataFrom == "Data From DB");
+            int fromRedis = resultList.Count(w => w.DataFrom == "Data From Redis");
 
             Console.WriteLine($"Data From DB :{fromDB}筆");
             Console.WriteLine($"Data From Redis :{fromRedis}筆");
